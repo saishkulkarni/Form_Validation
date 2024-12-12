eb1403e914def8430bc085584b9d4cae5ad742eb
saishkulkarni <saishkulkarni7@gmail.com>
2024-12-12 17:12:09 +0530

    WITH HTML EMAIL

--- src/main/java/org/jsp/registration_form/helper/MailHelper.java
+++ src/main/java/org/jsp/registration_form/helper/MailHelper.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import jakarta.mail.internet.MimeMessage;
 
@@ -14,12 +16,20 @@ public class MailHelper {
     @Autowired
     JavaMailSender sender;
 
+    @Autowired
+    TemplateEngine engine;
+
     public void sendEmail(Student student) {
         MimeMessage message=sender.createMimeMessage();
         MimeMessageHelper helper=new MimeMessageHelper(message);
         try{
         helper.setFrom("saishkulkarni7@jsp.com","Head Master");
-        helper.setText("Admission Successfull!!! Thank you for Creaing Account");
+       
+        Context context=new Context();
+        context.setVariable("student", student);
+
+        String text=engine.process("email-template.html", context);
+        helper.setText(text,true);
         helper.setSubject("Registration Successful");
         helper.setTo(student.getEmail());
         }catch(Exception e) {
